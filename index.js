c48c2d17af1467e58d7b0387323c62a7e0b15d23
exiondew <exiondew@gmail.com>
2024-07-13 13:51:46 +0300

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import "dotenv/config";
+// ---- ----
+import connectDB from "./db.js";
 
+//
+connectDB();
 const app = express();
 const port = process.env.PORT;
+//
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("index");
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
